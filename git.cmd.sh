052448e44b84a2935e9f47c25e7b9695a092eddf
Henry <henry.pham@alpaca.vn>
2024-09-25 09:41:30 +0700

    build : add .gitkeep and git init clone cmd

---
+++ git.cmd.sh
@@ -0,0 +1,3 @@
+git clone https://gitlab.com/henry-microservice/workspace/shared.git workspace/shared
+git clone https://gitlab.com/henry-microservice/workspace/infrastructure.git workspace/infrastructure
+git clone https://gitlab.com/henry-microservice/workspace/auth-service.git workspace/auth-service

4d6d118a3caac6e6fc6151b3860a9602494db4e1
Henry <henry.pham@alpaca.vn>
2025-01-08 14:56:44 +0700

    build: init project v2.1

--- git.cmd.sh
+++ git.cmd.sh
@@ -1,3 +1,6 @@
 git clone https://gitlab.com/henry-microservice/workspace/shared.git workspace/shared
 git clone https://gitlab.com/henry-microservice/workspace/infrastructure.git workspace/infrastructure
 git clone https://gitlab.com/henry-microservice/workspace/auth-service.git workspace/auth-service
+git clone https://gitlab.com/henry-microservice/workspace/movie-service.git workspace/movie-service
+git clone https://gitlab.com/henry-microservice/workspace/showtime-service.git workspace/showtime-service
+git clone https://gitlab.com/henry-microservice/workspace/ticket-service.git workspace/ticket-service

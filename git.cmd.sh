74bf6744babe1e1839b032767327fc04b9ddc762
henry.pham <phamvanhieu2106@gmail.com>
2025-02-22 11:38:50 +0700

    update git.cmd.sh

--- git.cmd.sh
+++ git.cmd.sh
@@ -1,5 +1,5 @@
-git clone https://gitlab.com/henry-microservice/workspace/shared.git workspace/shared
-git clone https://gitlab.com/henry-microservice/workspace/infrastructure.git workspace/infrastructure
+git clone https://gitlab.com/henry-microservice/workspace/shared.git workspace/1-shared
+git clone https://gitlab.com/henry-microservice/workspace/infrastructure.git workspace/2-infrastructure
 git clone https://gitlab.com/henry-microservice/workspace/auth-service.git workspace/auth-service
 git clone https://gitlab.com/henry-microservice/workspace/movie-service.git workspace/movie-service
 git clone https://gitlab.com/henry-microservice/workspace/showtime-service.git workspace/showtime-service

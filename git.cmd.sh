cd706c4745adc4988f002be38ddeba9a58b786ce
Pham Van Hieu <100674175+phamhieu2106@users.noreply.github.com>
2025-06-22 15:46:43 +0700

    Update git.cmd.sh

--- git.cmd.sh
+++ git.cmd.sh
@@ -1,6 +1,6 @@
-git clone https://gitlab.com/henry-microservice/workspace/shared.git workspace/1-shared
-git clone https://gitlab.com/henry-microservice/workspace/infrastructure.git workspace/2-infrastructure
-git clone https://gitlab.com/henry-microservice/workspace/auth-service.git workspace/auth-service
-git clone https://gitlab.com/henry-microservice/workspace/movie-service.git workspace/movie-service
-git clone https://gitlab.com/henry-microservice/workspace/showtime-service.git workspace/showtime-service
-git clone https://gitlab.com/henry-microservice/workspace/ticket-service.git workspace/ticket-service
+git clone https://github.com/phamhieu2106/base-microservice.workspace.Shared.git workspace/1-shared
+git clone https://github.com/phamhieu2106/base-microservice.workspace.Infrastructure.git workspace/2-infrastructure
+# git clone https://gitlab.com/henry-microservice/workspace/auth-service.git workspace/auth-service
+# git clone https://gitlab.com/henry-microservice/workspace/movie-service.git workspace/movie-service
+# git clone https://gitlab.com/henry-microservice/workspace/showtime-service.git workspace/showtime-service
+# git clone https://gitlab.com/henry-microservice/workspace/ticket-service.git workspace/ticket-service

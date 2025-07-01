43aef9aec7b6ef043411e40dc69126cb5b6055d0
Henry <henry.pham@alpaca.vn>
2025-07-01 10:10:06 +0700

    build: update base auth service

--- git.cmd.sh
+++ git.cmd.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/phamhieu2106/base-microservice.workspace.Shared.git workspace/1-shared
 git clone https://github.com/phamhieu2106/base-microservice.workspace.Infrastructure.git workspace/2-infrastructure
+git clone https://github.com/phamhieu2106/base-microservice.workspace.Auth-service.git workspace/auth-service
 
